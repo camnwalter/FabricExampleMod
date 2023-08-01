86c319467e4a49875bf9fd63690be024f1441f5f
Cam Walter <cam.n.walter@gmail.com>
2023-08-01 01:08:50 -0500

    convert to kotlin buildscripts, make client only, add kotlin

--- src/main/java/com/example/mixin/MinecraftClientMixin.java
+++ src/main/java/com/example/mixin/MinecraftClientMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin.client;
+package com.example.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,9 +7,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class ExampleClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void run(CallbackInfo info) {
-		// This code is injected into the start of MinecraftClient.run()V
-	}
+public class MinecraftClientMixin {
+    @Inject(method = "run", at = @At("HEAD"))
+    private void exampleMod$onGameLoad(CallbackInfo ci) {
+        System.out.println("Game Loaded");
+    }
 }
